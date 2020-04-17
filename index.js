dd8f43cc74d71d2e0e9398aec7f8c82a5c913890
Mateo Barreiros <matbarreiros@gmail.com>
2020-04-16 21:14:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ const spreadsheetId = "1Fh6NwzEtnGFtkRPcssIcKvLjNG-CyFVNJyrsBopyA4Q";
 const mensaje = "";
 const app = express();
 
-express()
-  .use(express.json())
-  .use(express.static(path.join(__dirname, "public")))
-  .set("views", path.join(__dirname, "views"))
-  .set("view engine", "ejs")
-  .get("/", (req, res) => res.render("pages/index"))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
 app.get("/", (req, res) => {
   res.send("noes");
 });
